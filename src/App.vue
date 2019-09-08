2710fabbf25b6a32b5bf56ea2ab0718a81fe7679
Shakil Alam <itxshakiil@gmail.com>
2019-09-08 15:29:59 +0530

    Initial  Commit

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Calculator></Calculator>
+    <a href="https://github.com/Itxshakiil/vue-calculator-app">View Code</a>
+    <a class="float-btn" href="mailto:itxshakiil@gmail.com?Subject=An%20issue%20found%20in%20Vue%Calculator%20App">Report an Issue</a>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calculator from './components/Calculator.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Calculator
   }
 }
 </script>
 
 <style>
+*{
+  box-sizing:border-box;
+}
+body{
+  background:#099;
+  position:relative;
+}
+a{
+  display:inline-block;
+  margin-top:10px;
+  text-decoration:none;
+  color:#ddd;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +38,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  box-sizing: border-box;
+}
+.float-btn{
+  padding:.4rem;
+  position:fixed;
+  right:0;
+  bottom:0;
 }
 </style>
---
+++ src/components/Calculator.vue
@@ -0,0 +1,119 @@
+<template>
+  <div>
+    <div class="calculator">
+      <div class="screen">
+      <input type="text" align="right" v-model="result">
+      <input type="text" align="right" v-model="num2">
+      </div>
+      <div class="buttons">
+        <button class="key" @click="addnum(7)">7</button>
+        <button class="key" @click="addnum(8)">8</button>
+        <button class="key"  @click="addnum(9)">9</button>
+        <button class="key" @click="addoperator('divide')">/</button>
+        <button class="key" @click="addnum(4)">4</button>
+        <button class="key" @click="addnum(5)">5</button>
+        <button class="key" @click="addnum(6)">6</button>
+        <button class="key" @click="addoperator('multiply')">&times;</button>
+        <button class="key" @click="addnum(1)">1</button>
+        <button class="key" @click="addnum(2)">2</button>
+        <button class="key" @click="addnum(3)">3</button>
+        <button class="key" @click="addoperator('sub')">-</button>
+        <button class="key" @click="addnum(0)">0</button>
+        <button class="key" @click="reset()">C</button>
+        <button class="key" @click="calculate()">=</button>
+        <button class="key" @click="addoperator('sum')">+</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Calculator",
+  data() {
+      return {
+          result:0,
+          num2:0,
+          operator:"sum",
+          lastkeyop:false,
+      }
+  },
+  methods: {
+      addnum:function(num){
+          this.num2 = (this.num2 * 10) + num;
+          this.lastkeyop=false;
+      },
+      addoperator:function(operator){ 
+          if(! this.lastkeyop){
+              this.calculate();
+            this.lastkeyop=true;
+          }
+          this.operator = operator;
+          // If everything is ok
+         
+      },
+      calculate:function(){
+          if(this.operator == "sum"){
+              this.result= this.result + this.num2;
+          }
+          if(this.operator == "sub"){
+              this.result= this.result - this.num2;
+          }
+          if(this.operator == "multiply"){
+              this.result= this.result * this.num2;
+          }
+          if(this.operator == "divide"){
+              if(this.num2 != 0 && this.num2 != null){
+              this.result= this.result / this.num2;
+              }else{
+                  alert('Can not Divide to zero.');
+              }
+          }
+          this.num2=null;
+      },
+      reset:function(){
+          this.result=0;
+          this.num2=0;
+          this.operator="sum";
+      }
+  },
+};
+</script>
+
+<style>
+.calculator {
+    max-width:300px;
+    margin:auto;
+    perspective: 1000;
+    box-shadow: 0px 0px 19px rgba(0,0,0,0.45);
+}
+.screen{
+    width:100%;
+}
+.buttons {
+  display: grid;
+  grid-template-rows: repeat(4, 1fr);
+  grid-template-columns: repeat(4, 1fr);
+}
+.key{
+    font-size:2rem;
+    padding:1rem;
+    width:75px;
+    height:75px;
+    background: #fff;
+    border:none;
+    border:1px solid rgba(0,0,0,0.45);
+}
+input{
+    background: #333;
+    color:#fff;
+    width:100%;
+    padding:0.5rem 1rem;
+    border:none;
+    text-align: right;
+    font-size: 1.3rem;
+}
+input:first-child{
+    font-weight:bold;
+}
+</style>
